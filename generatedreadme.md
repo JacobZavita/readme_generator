e5de4bdf752b50da1a3f2932b0e04973c80fcd8c
Jacob Zavita <jacobzavita@gmail.com>
2021-06-03 18:45:05 -0700

    format headings

--- generatedreadme.md
+++ generatedreadme.md
@@ -1,9 +1,20 @@
-# Awesome project
-## rwtgwtb5ebetyb
-## wrthwrtbrwb
-## rbyheryhberthg
-## wrthwrthwte
-## wrthwrth
-## Open
-## wrbwrtbhrtg
-## rwth
+# Great Project
+https://github.com/difbse/Great Project
+## Description
+does great things
+
+# Table of Contents
+* [Installation](#installation)
+# Installation
+click here
+# Usage
+wriogfbewirgbw
+# License
+Open
+# Contributing
+qeirfhbeqirgbqer
+# Tests
+erqigb3ribf23
+# Questions
+difbse
+# ofbwrfb3
--- readme.js
+++ readme.js
@@ -21,14 +21,15 @@ const readmeFile = {
   value: '',
   create (readme) {
     this.value += `# ${readme.title}\n`
-    this.value += `## ${readme.description}\n`
-    this.value += `## ${readme.installation}\n`
-    this.value += `## ${readme.usage}\n`
-    this.value += `## ${readme.contributing}\n`
-    this.value += `## ${readme.tests}\n`
-    this.value += `## ${readme.license}\n`
-    this.value += `## ${readme.github}\n`
-    this.value += `## ${readme.email}\n`
+    this.value += `https://github.com/${readme.github}/${readme.title}\n`
+    this.value += `## Description\n${readme.description}\n\n# Table of Contents\n* [Installation](#installation)\n`
+    this.value += `# Installation\n${readme.installation}\n`
+    this.value += `# Usage\n${readme.usage}\n`
+    this.value += `# License\n${readme.license}\n`
+    this.value += `# Contributing\n${readme.contributing}\n`
+    this.value += `# Tests\n${readme.tests}\n`
+    this.value += `# Questions\n${readme.github}\n`
+    this.value += `# ${readme.email}\n`
   },
   get () {
     return this.value
